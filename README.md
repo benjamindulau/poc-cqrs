9a2a2215500e31054b472136c0210d04a52561b7
Benjamin Dulau <benjamin.dulau@anonymation.com>
2013-04-29 11:40:20 +0200

    Added concrete workflow in README

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ CQRS POC
 
 This is an attempt of [CQRS](http://martinfowler.com/bliki/CQRS.html) implementation in a Symfony 2 application.
 
-Here is the worflow:
+Here is the general worflow:
 
 ![Workflow](https://raw.github.com/benjamindulau/poc-cqrs/master/workflow.png)
+
+
+Registration Workflow
+---------------------
+
+![Workflow](https://raw.github.com/benjamindulau/poc-cqrs/master/registration_workflow.png)
--- src/Acme/UserBundle/Domain/Model/User/UserServiceInterface.php
+++ src/Acme/UserBundle/Domain/Model/User/UserServiceInterface.php
@@ -2,8 +2,15 @@
 
 namespace Acme\UserBundle\Domain\Model\User;
 
+use Acme\UserBundle\Web\Command\ChangeEmailCommand;
+use Acme\UserBundle\Web\Command\RegisterCommand;
+use Acme\UserBundle\Web\Command\VerifyEmailCommand;
 
 interface UserServiceInterface
 {
+    function register(RegisterCommand $command);
 
+    function changeEmail(ChangeEmailCommand $command);
+
+    function verifyEmail(VerifyEmailCommand $command);
 }
